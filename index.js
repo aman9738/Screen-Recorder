1a5438eb8368feebccc662a9de6ea905e62dbfb3
Aman Agarwal <112307869+aman9738@users.noreply.github.com>
2023-01-06 11:47:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let btn = document.querySelector(".record-btn")  
+
+ btn.addEventListener("click", async function () {  
+  
+  let stream = await navigator.mediaDevices.getDisplayMedia({video:true});  
+
+   let mediaRecorder = new MediaRecorder(stream)  
+
+   let chunks = []  
+   mediaRecorder.addEventListener('dataavailable', function(e) {  
+     chunks.push(e.data)  
+   })  
+
+   mediaRecorder.addEventListener('stop', function(){  
+    let blob = new Blob(chunks, {  
+      type: chunks[0].type  
+    });
+
+    let url = URL.createObjectURL(blob)  
+    let video = document.querySelector("video")  
+    video.src = url  
+    let a = document.createElement('a')  
+    a.href = url  
+    a.download = 'video.webm'  
+    a.click()  
+  })  
+
+   //we have to start the recorder manually  
+   mediaRecorder.start()  
+ })
